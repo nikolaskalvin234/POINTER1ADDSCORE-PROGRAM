de8f17237b4a5ca715a17573159f70c6344877af
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-08 22:38:07 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Addscore
+{
+    unsafe class Program
+    {
+        public static void Addscore(int* score)
+        {
+            *score = *score + 5;
+            Console.WriteLine($"Score diubah ke: {*score}");
+        }
+
+        static void Main()
+        {
+            int score = 0;
+
+            Console.WriteLine($"Score sebelum diubah: {score}");
+            Addscore(&score);
+            Addscore(&score);
+            Addscore(&score);
+            Addscore(&score);
+            Console.WriteLine($"Score setelah diubah: {score}");
+        }
+    }
+}
